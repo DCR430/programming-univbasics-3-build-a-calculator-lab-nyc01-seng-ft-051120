8af51b5cad5cb0755c46e36beb76400c8be4b894
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-23 18:40:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+puts num1+num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+puts num1-num2
+returns sum
 end
 
 def division(num1, num2)
-
+puts num2/num1
+returns sum
 end
 
 def multiplication(num1, num2)
-
+  puts num1*num2
+  returns sum
 end
 
 def modulo(num1, num2)
-
+  puts num2/num1
+  retunrs sum.%
 end
 
 def square_root(num)
